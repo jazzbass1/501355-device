fb286e8dbf188cd908847169c87961a8788e2ea2
jazzbass1 <andreysmorgonsky@gmail.com>
2017-12-15 15:52:22 +0700

    правки к финальной проверке

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
   var link = document.querySelector(".write-us-btn");
   var popup = document.querySelector(".modal");
   var close = popup.querySelector(".modal-close");
+  var name = popup.querySelector("[name=name]");
+
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
+    name.focus();
   });
   close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
-    name.focus();
+
   });
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
+      popup.classList.remove("modal-show");
       }
     }
   });
